e79593b3ddbcbf6c52e3db311cfccfe84d106e43
将貴 米川 <masaki.2.28.my.f@gmail.com>
2023-06-22 22:08:10 +0900

    プロジェクト ファイルを追加します。

---
+++ inifile_input/IniFile.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inifile_input
+{
+    public class IniFile
+    {
+        private Dictionary<string, Dictionary<string, string>> data;
+
+        public IniFile()
+        {
+            data = new Dictionary<string, Dictionary<string, string>>();
+        }
+
+        public void Load(string filePath)
+        {
+            string[] lines = File.ReadAllLines(filePath, Encoding.UTF8);
+            string currentSection = null;
+
+            foreach (string line in lines)
+            {
+                string trimmedLine = line.Trim();
+
+                if (trimmedLine.StartsWith("[") && trimmedLine.EndsWith("]"))
+                {
+                    currentSection = trimmedLine.Substring(1, trimmedLine.Length - 2);
+                    if (!data.ContainsKey(currentSection))
+                        data[currentSection] = new Dictionary<string, string>();
+                }
+                else if (!string.IsNullOrEmpty(trimmedLine))
+                {
+                    int equalsIndex = trimmedLine.IndexOf("=");
+                    if (equalsIndex >= 0)
+                    {
+                        string key = trimmedLine.Substring(0, equalsIndex).Trim();
+                        string value = trimmedLine.Substring(equalsIndex + 1).Trim();
+                        if (!string.IsNullOrEmpty(key))
+                            data[currentSection][key] = value;
+                    }
+                }
+            }
+        }
+
+        public string GetValue(string section, string key)
+        {
+            if (data.ContainsKey(section) && data[section].ContainsKey(key))
+                return data[section][key];
+
+            return null;
+        }
+    }
+}
---
+++ inifile_input/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using inifile_input;
+
+Console.WriteLine("Hello, World!");
+
+string filePath = "file.ini";
+IniFile iniFile = new IniFile();
+iniFile.Load(filePath);
+
+// 値の取得例
+string section = "test";
+string key = "key1";
+string value = iniFile.GetValue(section, key);
+if (value != null)
+{
+    Console.WriteLine($"[{section}] {key} = {value}");
+}
+else
+{
+    Console.WriteLine($"[{section}] {key} not found.");
+}
